69721785c03b13b6d9dac05317a811497333905c
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2023-01-19 22:30:47 +0100

    Good looking URL - htaccess configuration

--- public/index.php
+++ public/index.php
@@ -1,2 +1,11 @@
 <?php
-echo "<h1>HELLO my Friend!</h1>";
+
+//FRONT CONTROLLER
+
+echo "<h1>Hello my Friend!</h1>";
+
+echo 'Requested route = '.$_SERVER['QUERY_STRING'].'<br>'.'<br>';
+echo 'Other things from $_SERVER = '.$_SERVER['SERVER_NAME'].'<br>';
+echo 'Other things from $_SERVER = '.$_SERVER['DOCUMENT_ROOT'].'<br>';
+echo 'Other things from $_SERVER = '.$_SERVER['SCRIPT_NAME'].'<br>';
+
