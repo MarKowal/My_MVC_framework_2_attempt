4cc1aeb0803babb6c86fe1a0c2385652be4c16d7
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2023-01-22 20:47:26 +0100

    Extract controller and action by regex

--- Core/Router.php
+++ Core/Router.php
@@ -2,8 +2,9 @@
 
 class Router {
 
-    //routing table:
+    //routing table to tablica asocjacyjna:
     protected $routes = [];
+    protected $params = [];
 
     public function add($route, $params){
         //funkcja uzupełnia routing table
@@ -11,6 +12,42 @@ class Router {
     }
 
     public function getRoutes(){
+        //funkcja pobiera tablicę $routes
         return $this->routes;
+    }
+
+    public function match($url){
+        //funkcja porównuje URL z routes w routing table
+        //jeżeli URL pasuje, to zapisuje controller i action w zmiennej params
+        // 1. proste matchowanie - porównanie dwóch stringów
+        /*foreach ($this->routes as $route => $params){
+            if($url == $route){
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
+        */
+
+        // 2. zaawansowane porównanie czy URL pasuje do REGEXa:
+        $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+        
+        if(preg_match($reg_exp, $url, $matches)){
+            $params = [];
+            foreach($matches as $key => $match){
+                //$key to controller, a $match to action
+                if(is_string($key)){
+                    $params[$key] = $match;
+                }
+            }
+            $this->params = $params;
+            return true;
+        }
+        return false;
+    }
+
+    public function getParams(){
+        //funkcja pobiera tablicę $params
+        return $this->params;
     }
 }
--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 //FRONT CONTROLLER
 
-echo "<h1>Hello my Friend!</h1>";
+echo "<h1>Routing table</h1>";
 
 //echo 'Requested route = '.$_SERVER['QUERY_STRING'].'<br>'.'<br>';
 //echo 'Other things from $_SERVER = '.$_SERVER['SERVER_NAME'].'<br>';
@@ -13,4 +13,23 @@ require '../Core/Router.php';
 $router = new Router();
 //echo get_class($router);  //zwraca nazwę klasy danego obiektu
 
-$router->add
+/*
+$router->add('', ['controller' => 'Home', 'action'=>'index']);
+$router->add('posts', ['controller' => 'Posts', 'action'=>'index'] );
+$router->add('posts/new', ['controller' => 'Posts', 'action'=>'new'] );
+*/
+
+//wyświetlenie routing table:
+/*echo '<pre>';
+var_dump($router->getRoutes());
+echo '</pre>';*/
+
+//sprawdzanie czy URL pasuje z jakimś routem:
+$url = $_SERVER['QUERY_STRING'];
+if($router->match($url)){
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo "No route found for this URL = ".$_SERVER['QUERY_STRING'];
+}
