cd7fee97b29e9a6d4e30fcc6b0744c9924a42f73
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2023-01-29 20:36:13 +0100

    Matching by route-regex

--- Core/Router.php
+++ Core/Router.php
@@ -6,8 +6,21 @@ class Router {
     protected $routes = [];
     protected $params = [];
 
-    public function add($route, $params){
+    //1. proste dodawanie route ze stałymi parametrami:
+    public function add($route, $params = []){
         //funkcja uzupełnia routing table
+        //1.usuwa z route ukośnik
+        $reg_exp = '/\//';
+        $replacement = '\\/';
+        //$route = '{controller}/{action}';
+        $route = preg_replace($reg_exp, $replacement, $route);
+        //2.zamienia nawiasy {} na regexa
+        $reg_exp = '/\{([a-z]+)}/';
+        $replacement = '(?P<\1>[a-z-]+)';
+        $route = preg_replace($reg_exp, $replacement, $route);
+        //3.dodaje początek i koniec regexa zrobionego z routa
+        $route = '/^'.$route.'$/i';
+        //4.zapisuje routa-regexa w routing table
         $this->routes[$route] = $params; //params to controller i action
     }
 
@@ -29,19 +42,23 @@ class Router {
         return false;
         */
 
-        // 2. zaawansowane porównanie czy URL pasuje do REGEXa:
-        $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+        // 2. zaawansowane matchowanie - porównanie czy URL pasuje do REGEXa:
+        //założenie: struktura URL jest stała, czyli controller/akcja
+        //$reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
         
-        if(preg_match($reg_exp, $url, $matches)){
-            $params = [];
-            foreach($matches as $key => $match){
-                //$key to controller, a $match to action
-                if(is_string($key)){
-                    $params[$key] = $match;
+        foreach($this->routes as $route => $params){
+            //if(preg_match($reg_exp, $url, $matches)){
+            if(preg_match($route, $url, $matches)){
+                //$params = [];
+                foreach($matches as $key => $match){
+                    //$key to controller, a $match to action
+                    if(is_string($key)){
+                        $params[$key] = $match;
+                    }
                 }
+                $this->params = $params;
+                return true;
             }
-            $this->params = $params;
-            return true;
         }
         return false;
     }
--- public/index.php
+++ public/index.php
@@ -13,23 +13,69 @@ require '../Core/Router.php';
 $router = new Router();
 //echo get_class($router);  //zwraca nazwę klasy danego obiektu
 
-/*
+
 $router->add('', ['controller' => 'Home', 'action'=>'index']);
-$router->add('posts', ['controller' => 'Posts', 'action'=>'index'] );
-$router->add('posts/new', ['controller' => 'Posts', 'action'=>'new'] );
-*/
+//$router->add('posts', ['controller' => 'Posts', 'action'=>'index'] );
+//$router->add('posts/new', ['controller' => 'Posts', 'action'=>'new'] );
+$router->add('{controller}/{action}');
+$router->add('admin/{action}/{controller}');
+$router->add('products/{controller}/{action}');
 
 //wyświetlenie routing table:
-/*echo '<pre>';
-var_dump($router->getRoutes());
-echo '</pre>';*/
+echo '<pre>';
+    echo 'ROUTES:<br>';
+    var_dump($router->getRoutes());
+echo '</pre>';
 
 //sprawdzanie czy URL pasuje z jakimś routem:
 $url = $_SERVER['QUERY_STRING'];
 if($router->match($url)){
-    echo '<pre>';
+    echo '<pre>';    
+    echo 'PARAMETERS:<br>';
     var_dump($router->getParams());
     echo '</pre>';
 } else {
     echo "No route found for this URL = ".$_SERVER['QUERY_STRING'];
 }
+
+/*$reg_exp = "/ab(C)de(F)/";
+$replacement = '\1wz67 \2er89';
+$string = 'abCdeF';
+$result = preg_replace($reg_exp, $replacement, $string);
+echo "<p>$string</p>";
+echo "<p>$result</p>";
+*/
+
+/*
+$reg_exp = "/\//";
+$replacement = "\\/";
+$string = "{controller}/{action}";
+$result = preg_replace($reg_exp, $replacement, $string);
+echo "<p>$string</p>";
+echo "<p>$result</p>";
+echo '---';
+
+$reg_exp = '/\{([a-z]+)\}/';
+$replacement = '(?P<\1>[a-z-]+)';
+$result2 = preg_replace($reg_exp, $replacement, $result);
+echo "<p>$result</p>";
+echo "<p>$result2</p>";
+echo '---';
+
+$result3 = '/^'.$result2.'$/i';
+echo "<p>$result3</p>";
+
+echo '------------------';*/
+$reg_exp = '/\//';
+$replacement = '\\/';
+$route = '{controller}/{action}';
+$route = preg_replace($reg_exp, $replacement, $route);
+
+$reg_exp = '/\{([a-z]+)}/';
+$replacement = '(?P<\1>[a-z-]+)';
+$route = preg_replace($reg_exp, $replacement, $route);
+
+$route = '/^'.$route.'$/i';
+echo "<p>$route</p>";
+
+?>
