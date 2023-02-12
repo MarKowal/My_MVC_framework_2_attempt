efd58eff548fceb52bfb40d11dab4ffb4354dfac
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2023-02-12 18:21:19 +0100

    Config file

--- Core/Model.php
+++ Core/Model.php
@@ -3,6 +3,7 @@
 namespace Core;
 
 use PDO;
+use App\Config;
 
 abstract class Model{
 
@@ -18,7 +19,8 @@ abstract class Model{
             $password = '';
 
             try{
-                $db = new PDO("mysql:host=$host; dbname=$dbname; charset=utf8", $username, $password);
+                $dsn = 'mysql:host='.Config::DB_HOST.';dbname='.Config::DB_NAME.'; charset=utf8';
+                $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
                 //$stmt = $db->query('SELECT id, title, content FROM posts ORDER BY created_at');
                 //$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
